a3576baad909d0fd6b49cb1197882e0a333783e7
Bhuvan Bhatt <bbhatt@kitmanlabs.com>
2025-07-07 08:27:58 +0100

    Add L10n

---
+++ Generated/L10n.swift
@@ -0,0 +1,46 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
+public enum L10n {
+  /// Cancel
+  public static let cancel = L10n.tr("Localizable", "Cancel", fallback: "Cancel")
+  /// OK
+  public static let ok = L10n.tr("Localizable", "OK", fallback: "OK")
+  public enum Video {
+    public enum Comments {
+      /// Show more
+      public static let showMore = L10n.tr("Localizable", "Video.Comments.ShowMore", fallback: "Show more")
+    }
+  }
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
